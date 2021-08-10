9c8143a49b446b643052b13504b9d536bcc26df6
JanineZielman <janine.zielman@icloud.com>
2021-08-10 18:47:03 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$('.menu-toggle').on('click', function() {
+
+	$('.menu').toggleClass('open')
+
+  return false
+
+ })
+
+ $(document).ready(function() {
+
+    $('a[href=#top]').click(function(){
+        $('html, body').animate({scrollTop:0}, 'slow');
+        return false;
+    });
+
+});
